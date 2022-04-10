6e3090fdb89526989718563e1e6012f63bd121be
memo <mehmetbaki.123@gmail.com>
2022-04-10 19:02:28 +0300

    finally done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+const employee={}
+employee.name="Sam"
+employee.streetAddress="TR"
+
+//ok
+function updateEmployeeWithKeyAndValue(employee, key, value){
+        const emp={...employee}
+        emp[key]=value
+        
+
+        return emp
+
+    }
+
+
+
+//ok
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key]=value
+        return employee
+
+ }
+
+
+ function deleteFromEmployeeByKey(employee,key) {
+
+    const emp2={
+        ...employee
+    }
+     delete emp2[key]
+
+     return emp2
+     
+ }
+
+
+
+ function destructivelyDeleteFromEmployeeByKey(employee,key) {
+
+          delete employee[key]
+
+          return employee
+
+ }
